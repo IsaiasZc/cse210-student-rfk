ab2857e44dcdd762094791ec6efab26252a1b760
IsaiasZc <78005126+IsaiasZc@users.noreply.github.com>
2021-11-03 20:00:40 -0500

    eso tilin

--- rfk/game/draw_actors_action.py
+++ rfk/game/draw_actors_action.py
@@ -1,3 +1,18 @@
 from game.action import Action
 
 # TODO: Define the DrawActorsAction class here
+class DrawActorsAction(Action):
+    
+    
+    def __init__(self,output_service):
+        self.output_service = output_service
+    
+    def execute(self,cast):
+        robot = cast["robot"]
+        marquee = cast["marquee"]
+        artifacts = cast["artifact"]
+        self.output_service.clear_screen()
+        self.output_service.draw_actors(robot)
+        self.output_service.draw_actors(artifacts)
+        self.output_service.draw_actors(marquee)
+        self.output_service.flush_buffer()
---
+++ rfk/game/tilinpy.py
@@ -0,0 +1,7 @@
+dicti = {
+    "perrito":["mesa"],
+    "gato": ["table"]
+}
+for elemento in dicti.values():
+    for elementito in elemento:
+        print(elementito)
